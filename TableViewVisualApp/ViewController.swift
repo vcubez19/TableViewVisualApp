b5be92b198ed3d7ba3d94a85354710679876c71c
Vincent Cubit <vinniecubit@yahoo.com>
2021-09-28 19:15:26 -0500

    Initial Commit

---
+++ TableViewVisualApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewVisualApp
+//
+//  Created by Vincent Cubit on 9/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
